c6f9baf03ad1f9ccbebbe9cad0fea23061832a6a
Shafi Swapnil <shafiahmedswapnil6494@gmail.com>
2020-12-12 00:15:19 +0600

    add comments for future

--- 00-Using-the-new-attributes-and-methods/js/placeholder.js
+++ 00-Using-the-new-attributes-and-methods/js/placeholder.js
@@ -1,15 +1,19 @@
 // Custom placeholder function
 
 function makePlaceholders() {
+	// list of input types
 	$inputs = $("input[type=text],input[type=email],input[type=tel],input[type=number],input[type=url");
 
+	// loops through inputs
 	$inputs.each(
+		// adds placeholder text to placeholder field
 		function () {
 			var $this = jQuery(this);
 			this.placeholderVal = $this.attr("placeholder");
 			$this.val(this.placeholderVal);
 		})
 		.bind("focus", function () {
+			// when focused, clear out input field
 			var $this = jQuery(this);
 			var val = $.trim($this.val());
 			if (val == this.placeholderVal || val == "") {
@@ -17,6 +21,7 @@ function makePlaceholders() {
 			}
 		})
 		.bind("blur", function () {
+			// when blured, bring again the placeholder text
 			var $this = jQuery(this);
 			var val = $.trim($this.val());
 			if (val == this.placeholderVal || val == "") {
