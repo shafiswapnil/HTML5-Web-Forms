5c4505b9d73d85c9fdb53fb831eac4d7d1420ddd
Shafi Swapnil <shafiahmedswapnil6494@gmail.com>
2020-12-13 00:17:44 +0600

    selection api

--- 00-Using-the-new-attributes-and-methods/js/selection-api.js
+++ 00-Using-the-new-attributes-and-methods/js/selection-api.js
@@ -1,4 +1,17 @@
-function selectSomeText() {
-	var input = document.getElementById("Textbox");
-	input.selectionStart = parseInt(document.getElementById())
+function selectText() {
+	var input = document.getElementById("textbox");
+	input.selectionStart = parseInt(document.getElementById("starting").value);
+	input.selectionEnd = parseInt(document.getElementById("ending").value);
+}
+
+function selectText2() {
+	var input = document.getElementById("textbox");
+	var start = parseInt(document.getElementById("starting").value);
+	var end = parseInt(document.getElementById("ending").value);
+	input.selectionRange(start, end);
+}
+
+function showSelection() {
+	var input = document.getElementById("textbox");
+	alert("Starting:" + input.selectionStart + " End: " + input.selectionEnd);
 }
